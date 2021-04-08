9d8d54ece1ed4a22836c7d2ccf0a95288d504756
pintergreg <pintergreg@gmail.com>
2021-04-08 09:30:10 +0200

    Use XAML object from code

--- src/AutomatedCar/Views/CourseDisplayView.xaml.cs
+++ src/AutomatedCar/Views/CourseDisplayView.xaml.cs
@@ -8,11 +8,20 @@ namespace AutomatedCar.Views
         public CourseDisplayView()
         {
             this.InitializeComponent();
+            // In Avalonia, this is required to catch a XAML object
+            var scrollViewer = this.FindControl<ScrollViewer>("scrollViewer");
+            scrollViewer.ScrollChanged += OnScrollViewerScrollChanged;
+            // scrollViewer.ScrollToHorizontalOffset is not implemented in Avalonia
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void OnScrollViewerScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            // System.Console.WriteLine(e.OffsetDelta);
         }
     }
 }
--- src/AutomatedCar/Views/MainWindow.xaml.cs
+++ src/AutomatedCar/Views/MainWindow.xaml.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
