0fbb9c92943b0a4e80e1ce855d55ad9d0e3db1ec
pintergreg <pintergreg@gmail.com>
2021-09-15 13:58:22 +0200

    Remove scrolling xperiment code

--- src/AutomatedCar/Views/CourseDisplayView.xaml.cs
+++ src/AutomatedCar/Views/CourseDisplayView.xaml.cs
@@ -9,29 +9,11 @@ namespace AutomatedCar.Views
         public CourseDisplayView()
         {
             this.InitializeComponent();
-            // In Avalonia, this is required to catch a XAML object
-            // var scrollViewer = this.FindControl<ScrollViewer>("scrollViewer");
-            // var scrollViewer = this.Get<ScrollViewer>("scrollViewer");
-            // scrollViewer.ScrollChanged += OnScrollViewerScrollChanged;
-            // scrollViewer.ScrollToHorizontalOffset is not implemented in Avalonia
-
-            // scrollViewer.Offset = new Avalonia.Vector(2500, 1000);
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
         }
-
-        private void OnScrollViewerScrollChanged(object sender, ScrollChangedEventArgs e)
-        {
-            // System.Console.WriteLine(e.OffsetDelta);
-        }
-
-        // protected override void OnKeyDown(KeyEventArgs e)
-        // {
-        //     base.OnKeyDown(e);
-        //     System.Console.WriteLine(this.Get<ScrollViewer>("scrollViewer").Width);
-        // }
     }
 }
