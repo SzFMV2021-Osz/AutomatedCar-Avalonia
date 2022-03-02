cb5ce392cc3c58fe2dd3e78634a10e351b62e407
Akos Szabo-Gali <szabogaliakos@gmail.com>
2022-03-02 14:59:35 +0100

    Register DummySensor to VirtualFunctionBus

--- src/AutomatedCar/Models/AutomatedCar.cs
+++ src/AutomatedCar/Models/AutomatedCar.cs
@@ -14,7 +14,9 @@ namespace AutomatedCar.Models
         {
             this.virtualFunctionBus = new VirtualFunctionBus();
             this.dummySensor = new DummySensor(this.virtualFunctionBus);
+            this.virtualFunctionBus.RegisterComponent(this.dummySensor);
             this.radarSensor = new RadarSensor(this);
+            this.virtualFunctionBus.RegisterComponent(this.radarSensor);
             this.ZIndex = 10;
         }
 
@@ -25,6 +27,7 @@ namespace AutomatedCar.Models
         public PolylineGeometry Geometry { get; set; }
 
         public RadarSensor RadarSensor { get => this.radarSensor; }
+        public DummySensor DummySensor { get => this.dummySensor; }
 
         /// <summary>Starts the automated cor by starting the ticker in the Virtual Function Bus, that cyclically calls the system components.</summary>
         public void Start()
